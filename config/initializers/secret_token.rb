8552a96980d265e4f2d8094314a7c960d76b24d6
Kentaro Yositomi <yositomi@las.osakafu-u.ac.jp>
2013-09-02 11:08:25 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mathdrill::Application.config.secret_key_base = '6517db3dbadd3669b50419fe1ee8a6cb058449c9e98a0f38723512e16cc8b9b6e81a3c3f79103c3bd15f4c3dcf4e495fbb7d4416f8b7be86ac231b8c8d33d5f4'
