8bed010340fee1d20d82c3ffc4db01abfc20861d
Kentaro Yositomi <yositomi@las.osakafu-u.ac.jp>
2013-09-09 16:35:27 +0900

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します
   def full_title(page_title)
-    base_title = "Mathdrill Database Sharing Server"
+    base_title = "MathDrill Share App"
     if page_title.empty?
       base_title
     else
