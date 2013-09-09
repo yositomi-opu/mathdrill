53f777c772ada25ff65954f397a646bc80d2c81f
Kentaro Yositomi <yositomi@las.osakafu-u.ac.jp>
2013-09-09 13:58:10 +0900

    Commit before ch5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Mathdrill::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
+#  get "static_pages/help"
+#  get "static_pages/about"
+#  get "static_pages/contact"
+#  match '/', to: 'static_pages#home', via: 'get'
+  root '/static_pages#home'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
 
   resources :users
