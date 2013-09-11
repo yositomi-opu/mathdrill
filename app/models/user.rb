cf450735a88dad5b73f595997e369a000c0aca6f
Kentaro Yositomi <yositomi@las.osakafu-u.ac.jp>
2013-09-11 18:38:14 +0900

    EMAIL REGEXP updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 20 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_secure_password
+#  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 30 },
+  		    uniqueness: { case_sensitive: true }
+#  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z\d]+\.)+[a-z]{2,})\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
   		    format:   { with: VALID_EMAIL_REGEX },
 		    uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, length: { minimum: 6 }
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,10 +30,30 @@ describe User do
   end
 
   describe "when name is too long" do
-    before { @user.name = "a" * 21 }
+    before { @user.name = "a" * 31 }
     it { should_not be_valid }
   end    
 
+  describe "when name is already taken" do
+    before do
+      user_with_same_name = @user.dup
+      user_with_same_name.name = @user.name
+      user_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
