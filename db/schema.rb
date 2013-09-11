6afea62016914d5d5273f6b2fe4b280e59f34972
Kentaro Yositomi <yositomi@las.osakafu-u.ac.jp>
2013-09-11 15:12:21 +0900

    Make a basic User model with secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130905124844) do
+ActiveRecord::Schema.define(version: 20130911003511) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 20130905124844) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
     
@@ -14,24 +19,35 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About Us' }
     
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
   
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
     
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    click_link "Help"
+    click_link "Contact"
+    click_link "Home"
+    click_link "Sign up now!"
+  end
 end
 
